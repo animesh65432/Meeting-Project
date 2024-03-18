ceb162eadbd5c6c738fdf7205f9a772fcc3515fd
animesh65432 <kiranduttta234@gmail.com>
2024-03-18 10:39:40 +0530

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,21 @@
+import MeetupList from "../../components/meetups/MeetupList";
+
+const dummymeetups = [
+  {
+    id: 1,
+    title: "A first Meetup",
+    image:
+      "https://cdn.britannica.com/91/110191-050-7BCFD56B/Victoria-Memorial-Hall-Kolkata-India.jpg",
+    address: "Some address city",
+    description: "This is the Goot Meetup",
+  },
+];
+
 const Homepage = () => {
-  return <></>;
+  return (
+    <>
+      <MeetupList meetups={dummymeetups} />
+    </>
+  );
 };
 export default Homepage;
