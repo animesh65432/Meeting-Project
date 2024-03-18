becfa52b3d0c6a1c118ea2ac968f7620d0d3bb8f
animesh65432 <kiranduttta234@gmail.com>
2024-03-18 16:04:47 +0530

    get user details

--- src/pages/[meetupid]/index.js
+++ src/pages/[meetupid]/index.js
@@ -1,50 +1,73 @@
 import MeetupDetails from "../../../components/meetups/MeetupDetails";
+import { MongoClient, ObjectId } from "mongodb";
 
-const Meetupid = () => {
+const Meetupid = (props) => {
   return (
     <>
-      <MeetupDetails
-        image="https://s3.india.com/travel/wp-content/uploads/2017/10/Kolkata-city1.jpg"
-        title="Kolkata"
-        address="Kolkata"
-        description="kolkata rasta"
-      />
+      <MeetupDetails {...props.meetupprops} />
     </>
   );
 };
 
 export async function getStaticPaths() {
-  return {
-    paths: [
-      {
+  let client;
+  try {
+    client = await MongoClient.connect(
+      "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5"
+    );
+    const db = client.db();
+    const meetupCollections = db.collection("database");
+
+    const meetups = await meetupCollections.find({}, { _id: 1 }).toArray();
+    client.close();
+
+    return {
+      paths: meetups.map((obj) => ({
         params: {
-          meetupid: "m1",
+          meetupid: obj._id.toString(),
         },
-      },
-      {
-        params: {
-          meetupid: "m2",
-        },
-      },
-    ],
-    fallback: true,
-  };
+      })),
+      fallback: false,
+    };
+  } catch (error) {
+    console.error(error);
+    if (client) client.close();
+  }
 }
 
 export async function getStaticProps(context) {
   const meetupid = context.params.meetupid;
-  console.log(meetupid);
-  return {
-    props: {
-      meetupprops: {
-        image:
-          "https://s3.india.com/travel/wp-content/uploads/2017/10/Kolkata-city1.jpg",
-        title: "Kolkata",
-        address: "Kolkata",
-        description: "kolkata rasta",
+
+  try {
+    const client = await MongoClient.connect(
+      "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5"
+    );
+    const db = client.db();
+    const meetupCollections = db.collection("database");
+
+    const meetup = await meetupCollections.findOne({
+      _id: new ObjectId(meetupid),
+    });
+    client.close();
+
+    return {
+      props: {
+        meetupprops: {
+          image: meetup.image,
+          title: meetup.title,
+          address: meetup.address,
+          description: meetup.description,
+        },
       },
-    },
-  };
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        meetupprops: {},
+      },
+    };
+  }
 }
 
 export default Meetupid;
